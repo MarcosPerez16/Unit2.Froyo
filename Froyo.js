40b4c265e51eaee01bf8f9a2c4dab33690bed253
MarcosPerez16 <marcos.perez06@yahoo.com>
2023-10-04 09:40:03 -0700

    complete app

--- Froyo.js
+++ Froyo.js
@@ -1,1 +1,27 @@
-console.log("Working");
+//prompts user to input flavors with no commas or spaces
+// user types vanilla, vanilla, vanilla, strawberry, coffee, coffee
+
+const userInput = prompt(
+  "Please type the Froyo flavors you would like to order with commas and no spaces in between."
+);
+
+// splits input string into an array of flavors
+
+const flavors = userInput.split(",");
+
+//create a object to store flavors
+
+const flavorCount = {};
+
+//Count the number of flavors
+
+flavors.forEach((flavor) => {
+  if (flavorCount[flavor]) {
+    flavorCount[flavor]++;
+  } else {
+    flavorCount[flavor] = 1;
+  }
+});
+
+//logs everything to the console
+console.table(flavorCount);
